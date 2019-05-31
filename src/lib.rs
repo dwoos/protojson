06360acd2616161fe77af6ec8c96bb20ccb1de8d
Doug Woos <doug.woos@gmail.com>
2019-05-31 13:50:42 -0700

    better api

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,25 @@ use std::io::BufRead;
 
 mod proto;
 
-pub fn read_protobuf_message_as_json(
-    descriptors_path: String,
-    message: String,
-    input: &mut BufRead,
-) -> Result<JValue, Error> {
-    let mut file = fs::File::open(descriptors_path)?;
-    let proto = protobuf::parse_from_reader(&mut file)?;
-    let descriptors = Descriptors::from_proto(&proto);
-    let input = protobuf::CodedInputStream::from_buffered_reader(input);
-    let mut deserializer = Deserializer::for_named_message(&descriptors, &message, input)?;
-    let value = JValue::deserialize(&mut deserializer)?;
-    Ok(value)
+pub struct ProtobufToJson {
+    descriptors: Descriptors,
+}
+
+impl ProtobufToJson {
+    pub fn new(descriptors_path: &str) -> Result<Self, Error> {
+        let mut file = fs::File::open(descriptors_path)?;
+        let proto = protobuf::parse_from_reader(&mut file)?;
+        let descriptors = Descriptors::from_proto(&proto);
+        Ok(Self { descriptors })
+    }
+
+    pub fn to_json(&self, message_type: &str, input: &mut BufRead) -> Result<JValue, Error> {
+        let input = protobuf::CodedInputStream::from_buffered_reader(input);
+        let mut deserializer =
+            Deserializer::for_named_message(&self.descriptors, message_type, input)?;
+        let value = JValue::deserialize(&mut deserializer)?;
+        Ok(value)
+    }
 }
 
 #[cfg(test)]
@@ -28,7 +35,7 @@ mod tests {
     fn parse_simple_message() -> Result<(), failure::Error> {
         use protobuf::Message;
         use serde_json::json;
-        use serde_json::Value::{Array, Null};
+        use serde_json::Value::Null;
         use std::io::BufReader;
         let mut person = crate::proto::basic::Person::new();
         person.set_email("email@example.com".into());
@@ -41,11 +48,10 @@ mod tests {
         let descriptors_path = "examples/basic.pb";
         let message = ".Person";
         let mut input = BufReader::new(&person_bytes[..]);
-        let value = crate::read_protobuf_message_as_json(
-            descriptors_path.into(),
-            message.into(),
-            &mut input,
-        )?;
+
+        let p_to_j = crate::ProtobufToJson::new(descriptors_path)?;
+
+        let value = p_to_j.to_json(message, &mut input)?;
         assert_eq!(
             value,
             json!({ "email": "email@example.com", "id":  Null, "name": Null, "phones": [] })
